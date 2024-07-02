6e28f35954a11502f57a66bf27d9f2e491995d0c
chrisaddy <chris.william.addy@gmail.com>
2024-07-02 10:55:12 -0400

    host tests

--- src/host.rs
+++ src/host.rs
@@ -21,3 +21,35 @@ impl std::fmt::Display for Machine {
         write!(f, "{}-{}-{}", self.os, self.family, self.arch)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[cfg(target_os = "windows")]
+    fn test_windows() {
+        let machine = Machine::new();
+        assert_eq!(machine.os, "windows");
+        assert_eq!(machine.family, "unix");
+        assert_eq!(machine.arch, "x86_64");
+    }
+
+    #[test]
+    #[cfg(target_os = "linux")]
+    fn test_linux() {
+        let machine = Machine::new();
+        assert_eq!(machine.os, "linux");
+        assert_eq!(machine.family, "unix");
+        assert_eq!(machine.arch, "x86_64");
+    }
+
+    #[test]
+    #[cfg(target_os = "macos")]
+    fn test_macos() {
+        let machine = Machine::new();
+        assert_eq!(machine.os, "macos");
+        assert_eq!(machine.family, "unix");
+        assert_eq!(machine.arch, "aarch64");
+    }
+}
